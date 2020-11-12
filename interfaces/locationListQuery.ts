00ae95e3c0e42501b82638b9080531d3aa51a8d7
Burhan Kundakcioglu <burhan.k@me.com>
2020-11-12 06:17:31 +0000

    Update locationListQuery.ts

--- interfaces/locationListQuery.ts
+++ interfaces/locationListQuery.ts
@@ -3,9 +3,8 @@ export default interface LocationListPageQuery {
   sortby?: 'abc' | 'last' | 'near';
   lat?: number;
   lng?: number;
-  search?: string; // city, county etc.
+  location?: string; // city, county etc.
   category?: string; // convert it to string cat value after fetch all categories from an endpoint.
   category_id?: number;
   search?: string;
-  location?: string;
 }
