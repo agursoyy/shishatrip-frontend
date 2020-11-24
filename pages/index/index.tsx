05d4f59d9a14737179fa3df1dd3029bb0f5bc337
Gursoy <alptekin.gursoy@ceng.deu.edu.tr>
2020-11-24 19:10:58 +0300

    try

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -64,6 +64,8 @@ Home.pageConfig = {
 function isNumeric(x: any) {
   return parseFloat(x).toString() === x.toString();
 }
+
+/*
 Home.getInitialProps = async ({ store, pathname, query }: NextPageContext): Promise<IProps> => {
   const { page, sortby, lat, lng, category, search } = query;
   let pageQuery = 1,
@@ -100,7 +102,6 @@ Home.getInitialProps = async ({ store, pathname, query }: NextPageContext): Prom
   }
   console.log(categoryObj);
 
-  /*
   await store.dispatch(
     fetchInıtData({
       page: pageQuery,
@@ -110,7 +111,7 @@ Home.getInitialProps = async ({ store, pathname, query }: NextPageContext): Prom
       category: categoryObj?.id,
       search: search?.toString(),
     }) as any,
-  ); */
+  );
 
   return {
     query: {
@@ -123,16 +124,75 @@ Home.getInitialProps = async ({ store, pathname, query }: NextPageContext): Prom
       search: search?.toString(),
     },
   };
-};
+}; */
 
 //export default connect(Home);
 
-/*
-export const getServerSideProps = wrapper.getServerSideProps(async ({ store, req, res }) => {
-  console.log('2. Page.getServerSideProps uses the store to dispatch things');
-  await store.dispatch(login({ email: 'email', password: 'password' }) as any);
-  //await store.dispatch(fetchInıtData() as any);
-  return { props: { custom: 'alptekin' } };
-});*/
+export const getServerSideProps = wrapper.getServerSideProps(async ({ store, req, res, query }) => {
+  const { page, sortby, lat, lng, category, search } = query;
+  let pageQuery = 1,
+    sortByQuery: 'abc' | 'last' | 'near' | undefined,
+    latQuery,
+    lngQuery,
+    categoryObj,
+    searchQuery;
+
+  if (page && isNumeric(page.toString())) {
+    pageQuery = parseInt(page.toString());
+  }
+
+  if (sortby) {
+    let temp = sortby.toString();
+    if (temp === 'abc' || temp === 'last' || temp === 'near') {
+      sortByQuery = temp;
+    }
+  }
+  if (lat && isNumeric(lat.toString())) {
+    latQuery = parseFloat(lat.toString());
+  }
+  if (lng && isNumeric(lng.toString())) {
+    lngQuery = parseFloat(lng.toString());
+  }
+  if (category) {
+    let catStr = category.toString();
+    const { locations } = store.getState() as RootState;
+    const { categories } = locations;
+    categoryObj = categories.categories.find(
+      (cat: any) => cat.name.toLowerCase() === catStr.toLowerCase(),
+    );
+  }
+  await store.dispatch(fetchCategories() as any);
+
+  await store.dispatch(
+    fetchInıtData(
+      JSON.parse(
+        JSON.stringify({
+          page: pageQuery,
+          sortby: sortByQuery,
+          lat: latQuery,
+          lng: lngQuery,
+          category: categoryObj?.id,
+          search: search?.toString(),
+        }),
+      ),
+    ) as any,
+  );
+
+  return {
+    props: {
+      query: JSON.parse(
+        JSON.stringify({
+          page: pageQuery,
+          sortby: sortByQuery,
+          lat: latQuery,
+          lng: lngQuery,
+          category: categoryObj?.name.toLowerCase(),
+          category_id: categoryObj?.id,
+          search: search?.toString(),
+        }),
+      ),
+    },
+  };
+});
 
 export default Home;
