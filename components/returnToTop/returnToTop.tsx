7496b9b8b0cc632b9f4662573476d345057df508
Gursoy <alptekin.gursoy@ceng.deu.edu.tr>
2020-12-02 11:53:44 +0300

    handle returnToTop using only css

--- components/returnToTop/returnToTop.tsx
+++ components/returnToTop/returnToTop.tsx
@@ -4,52 +4,17 @@ import './returnToTop.scss';
 const ReturnToTop = () => {
   const returnToTopRef = useRef<HTMLDivElement | any>(null);
 
-  function fadeIn(interval: number) {
-    if (
-      returnToTopRef &&
-      returnToTopRef.current &&
-      returnToTopRef.current.style.display == 'none'
-    ) {
-      returnToTopRef.current.style.display = 'inline';
-      returnToTopRef.current.style.opacity = '0';
-      var opacity = 0;
-      var intervalID = setInterval(function () {
-        if (opacity < 1) {
-          opacity = opacity + 0.1;
-          (returnToTopRef.current as any).style.opacity = opacity.toString();
-        } else {
-          clearInterval(intervalID);
-        }
-      }, interval);
-    }
-  }
-  function fadeOut(interval: number) {
-    if (
-      returnToTopRef &&
-      returnToTopRef.current &&
-      returnToTopRef.current.style.display != 'none'
-    ) {
-      var opacity = 1;
-      var intervalID = setInterval(function () {
-        if (opacity > 0) {
-          opacity = opacity - 0.1;
-          (returnToTopRef.current as any).style.opacity = opacity.toString();
-        } else {
-          clearInterval(intervalID);
-          (returnToTopRef.current as any).style.display = 'none';
-        }
-      }, interval);
-    }
-  }
   useEffect(() => {
     window.addEventListener('scroll', function (e) {
       var body = document.body;
       var docElem = document.documentElement;
       var height = window.pageYOffset || docElem.scrollTop || body.scrollTop;
       if (height >= 450) {
-        fadeIn(50);
+        // fadeIn(50);
+        returnToTopRef.current && returnToTopRef.current.classList.add('scrolled');
       } else {
-        fadeOut(20);
+        // fadeOut(20);
+        returnToTopRef.current && returnToTopRef.current.classList.remove('scrolled');
       }
     });
 
@@ -60,7 +25,7 @@ const ReturnToTop = () => {
     }
   }, []);
   return (
-    <a id="return-to-top" style={{ display: 'none' }} ref={returnToTopRef}>
+    <a id="return-to-top" ref={returnToTopRef}>
       <i className="fa fa-chevron-up"></i>{' '}
     </a>
   );
